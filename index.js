5cc1a9b8be0886cabaece3752bc4097eb6584207
ianimbuga <imbugaian20@gmail.com>
2024-09-26 21:49:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+  
+    for (const name of names) {
+      const message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
